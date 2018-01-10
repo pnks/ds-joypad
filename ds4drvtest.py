f373b583801765373ccd76de755a616d384d8f03
Dion <312225@student.mboutrecht.nl>
2018-01-10 13:29:19 +0000

    Updated ds4drvtest. renamed files

--- ds4drvtest.py
+++ ds4drvtest.py
@@ -4,14 +4,11 @@ import sys,time
 
 # grovepi init
 PIN_RELAY = 2
-PIN_CHAINLED = 3
-NUM_CHAINLED = 1
-PIN_LEDBAR = 4
+PIN_CONNECTLED = 3
 
 import grovepi
 grovepi.pinMode(PIN_RELAY,"OUTPUT");
-grovepi.pinMode(PIN_CHAINLED,"OUTPUT");
-grovepi.chainableRgbLed_init(PIN_CHAINLED, NUM_CHAINLED)
+grovepi.pinMode(PIN_CONNECTLED,"OUTPUT");
 
 # picoborg init
 import PicoBorgRev
@@ -21,7 +18,7 @@ PBR.Init()
 
 class Program(MainLoop):
     def initialize(self):
-        self.pad = gamepad.DS3Controller()
+        self.pad = gamepad.DS4Controller()
 
         self.emstop = False
 
@@ -30,6 +27,9 @@ class Program(MainLoop):
         self.pad.on_disconnect += self.on_disconnect
         self.pad.on_buttondown += self.on_buttondown
         self.pad.on_buttonup += self.on_buttonup
+
+        grovepi.digitalWrite(PIN_CONNECTLED,1)
+
 
     def on_start(self):
         print("Waiting for gamepad...")
@@ -53,14 +53,14 @@ class Program(MainLoop):
         
     def on_buttondown(self,name,value):
         print("\nButton '{0}' pressed".format(name))
-        if name == "SELECT":
+        if name == "SHARE":
             self.emstop = True
             print("Emergency Stop")
             PBR.SetMotor1(0)
             PBR.SetMotor2(0)
             print("Disabling Weapon")
             grovepi.digitalWrite(PIN_RELAY,0)
-        elif name == "START":
+        elif name == "OPTIONS":
             self.emstop = False
             print("Emergency Stop Released")
             
@@ -68,19 +68,21 @@ class Program(MainLoop):
             if name == "TRI":
                 print("Enabling Weapon")
                 grovepi.digitalWrite(PIN_RELAY,1)            
-
-    def on_buttonup(self,name,value):
-        print("\nButton '{0}' released".format(name))
-        if not self.emstop:
-            if name == "TRI":
+            elif name == "O":
                 print("Disabling Weapon")
                 grovepi.digitalWrite(PIN_RELAY,0)
 
+    def on_buttonup(self,name,value):
+        print("\nButton '{0}' released".format(name))
+
     def on_connect(self,padname):
         print("\nJoystick '{0}' connected".format(padname))
+        grovepi.digitalWrite(PIN_CONNECTLED,0)
+
 
     def on_disconnect(self,padname):
         print("\nJoystick '{0}' disconnected".format(padname))
+        grovepi.digitalWrite(PIN_CONNECTLED,1)
 
         print("Disabling motors")
         PBR.SetMotor1(0)
